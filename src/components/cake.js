7bb48e452e53fb3d058f130581d6ff226a8d6080
nmahmood <mahmud.nafisa02@gmail.com>
2023-07-17 23:20:28 -0400

    footer done, carousel done, need to figure out dark mode css and logic

---
+++ src/components/cake.js
@@ -0,0 +1,29 @@
+import carousel1 from '../images/CAROUSEL-01.png';
+import carousel2 from '../images/CAROUSEL-02.png';
+import carousel3 from '../images/CAROUSEL-03.png';
+import carousel4 from '../images/CAROUSEL-04.png';
+import carousel5 from '../images/CAROUSEL-05.png';
+
+export const cakes = [
+  {
+    image: carousel1,
+    title:'carousel1',
+  },
+  {
+    image: carousel2,
+    title:'carousel2',
+  },
+  {
+    image: carousel3,
+    title:'carousel3',
+  },
+  {
+    image: carousel4,
+    title:'carousel4',
+  },
+  {
+    image: carousel5,
+    title:'carousel5',
+  }
+ ]
+
